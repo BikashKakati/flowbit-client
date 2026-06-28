786cfd1378dc2c580e74aceba9c64a7424ffbd51
BikashKakati <bikashkakati796@gmail.com>
2026-06-28 16:35:48 +0530

    [UPDATE] Height design change

--- src/components/layouts/PublicLayout.tsx
+++ src/components/layouts/PublicLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function PublicLayout() {
     return (
-        <div className="min-h-screen bg-slate-950 text-white font-sans selection:bg-indigo-500/30">
+        <div className="h-full bg-slate-950 text-white font-sans selection:bg-indigo-500/30">
             <Outlet />
         </div>
     );
